2bca1d892e7a2114fcc8ae5d29e832735099655b
erichhugo-beep <erich.hugo@gmail.com>
2025-09-13 19:40:45 +0200

    feat: add dummy workload that runs on idle

---
+++ workloads/dummy/run.py
@@ -0,0 +1,11 @@
+﻿import time
+
+def run():
+    print("[dummy] workload started")
+    try:
+        while True:
+            # Simulate CPU work
+            _ = sum(i*i for i in range(10000))
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("[dummy] workload stopped")
